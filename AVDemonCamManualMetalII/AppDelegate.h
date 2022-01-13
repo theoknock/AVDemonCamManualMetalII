28be574a463c94f64912891d77fb6bf120402281
James Alan Bush <jamesalanbush1972@gmail.com>
2022-01-13 04:08:14 -0500

    Initial Commit

---
+++ AVDemonCamManualMetalII/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  AVDemonCamManualMetalII
+//
+//  Created by Xcode Developer on 1/13/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
